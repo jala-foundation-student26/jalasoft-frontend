ec1c6bff19a9ca3e3ced2e9ed1438015d12e0aeb
gustavocotalima <student26@jala-foundation.org>
2023-11-18 12:36:53 -0300

    user list

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,13 +1,40 @@
 import UserCard from '@/components/user-card'
 
-export default function List() {
+interface User {
+  id: number
+  email: string
+  first_name: string
+  last_name: string
+  avatar: string
+}
+
+async function getUsers(): Promise<User[]> {
+  const response = await fetch('https://reqres.in/api/users', {
+    cache: 'no-store',
+  })
+
+  const { data: users } = await response.json()
+
+  return users
+}
+
+export default async function List() {
+  const users = await getUsers()
+  console.log(users)
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 px-4 py-8 sm:px-8 gap-12">
-      <UserCard />
-      <UserCard />
-      <UserCard />
-      <UserCard />
-      <UserCard />
+      {users.map((user) => {
+        return (
+          <UserCard
+            key={user.id}
+            id={user.id}
+            avatar={user.avatar}
+            email={user.email}
+            firstName={user.first_name}
+            lastName={user.last_name}
+          />
+        )
+      })}
     </div>
   )
 }
--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -1,11 +1,25 @@
 import Image from 'next/image'
 import { Trash, PencilSimple } from '@phosphor-icons/react/dist/ssr'
 
-export default function UserCard() {
+interface UserCardProps {
+  id: number
+  email: string
+  firstName: string
+  lastName: string
+  avatar: string
+}
+
+export default function UserCard({
+  id,
+  email,
+  firstName,
+  lastName,
+  avatar,
+}: UserCardProps) {
   return (
     <div className="flex w-full gap-4 p-4 border-2 rounded-md border-zinc-950">
       <Image
-        src="/windowsxp-bg.jpg"
+        src={avatar}
         width={128}
         height={128}
         className="w-32 h-32 rounded-md"
@@ -14,13 +28,13 @@ export default function UserCard() {
       />
       <div className="flex-1 flex flex-col gap-4 ">
         <p>
-          Name: <span>Mariana</span>
+          Name: <span>{firstName}</span>
         </p>
         <p>
-          Last Name: <span>Bellot</span>
+          Last Name: <span>{lastName}</span>
         </p>
         <p>
-          Email: <span>maribellot@mail.com</span>
+          Email: <span>{email}</span>
         </p>
         <div className="flex justify-center items-center gap-4">
           <Trash size={32} className="text-zinc-950" weight="fill" />
